f898e8882a97cfa2a86d8b35fc15200cbe2545ca
Steve Cheruiyot <skc2143@columbia.edu>
2015-02-18 02:49:24 -0500

    adding cache.js

--- hw2/lib/cache/cache.js
+++ hw2/lib/cache/cache.js
@@ -1,4 +1,14 @@
 (function () {
   'use strict';
-
+  function cache (someFunction) {
+    var args;
+    var result;
+    return function (a, b) {
+      if (args !== JSON.stringify(arguments)) {
+        args = JSON.stringify(arguments);
+        result = someFunction(a, b);
+      }
+      return result;
+    };
+  }
 })();
