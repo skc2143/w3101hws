64d74bbe0e35d086e356da1dc41e6b63e1e9c388
Steve Cheruiyot <skc2143@columbia.edu>
2015-02-28 23:56:30 -0500

    added functionality to DI.js

--- hw3/lib/di/DI.js
+++ hw3/lib/di/DI.js
@@ -1,6 +1,53 @@
 (function () {
   'use strict';
-
-  module.exports = {};
-
+  var myModule = (function () {
+    var name,
+        dependencies = {},
+        register,
+        inject,
+        getRegisteredFunc;
+    register = function (name, func) {
+      dependencies[name] = func;
+    };
+    inject = function (func) {
+      //regex from angular source code
+      var regex =/^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+      var args = func.toString().match(regex)[1].replace(/ /g, '').split(',');
+      var deps = [];
+      for (var i = 0; i < args.length; i++) {
+        deps.push(dependencies[args[i]]);
+      }
+      var a = func;
+      var b = func.apply(a, deps);
+      return b;
+    };
+    getRegisteredFunc = function () {
+      var temp = [];
+      for (var i in dependencies) {
+        temp.push(dependencies[i]);
+      }
+      return temp.join(',');
+    }
+    var module = {
+      name: name,
+      dependencies: dependencies,
+      register: register,
+      inject: inject,
+      getRegisteredFunc: getRegisteredFunc
+    };
+    return module;
+  })();
+  module.exports = {
+    modules: {},
+    module: function (name, dependencies) {
+      myModule.name = name;
+      if (dependencies.length === 0) {
+        return myModule;
+      }
+      for (var i in dependencies) {
+        myModule.dependencies = dependencies;
+      }
+      return myModule;
+    }
+  };
 })();
